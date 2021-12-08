906b578ab1e3f1d6a5f1d0560227c954293d6430
prtzl <matej.blagsic@protonmail.com>
2021-12-08 21:20:29 +0100

    Manage vscode from vscode.nix not home.nix

---
+++ home/vscode.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ...}:
+
+{
+  home.packages = with pkgs; [
+    vscode-with-extensions
+    vscode-extensions.ms-vsliveshare.vsliveshare 
+    vscode-extensions.ms-python.python
+    vscode-extensions.ms-vscode.cpptools
+    vscode-extensions.donjayamanne.githistory
+    vscode-extensions.file-icons.file-icons
+  ];
+}
