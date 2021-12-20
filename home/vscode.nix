4ed3bc8b18fb8f4d8f7192037b2a1733caeca9a5
prtzl <matej.blagsic@protonmail.com>
2021-12-20 22:05:48 +0100

    Experiment with vscode, vscode-fhs seems to be working for my extensions

--- home/vscode.nix
+++ home/vscode.nix
@@ -2,11 +2,12 @@
 
 {
   home.packages = with pkgs; [
-    vscode-with-extensions
-    vscode-extensions.ms-vsliveshare.vsliveshare 
-    vscode-extensions.ms-python.python
-    vscode-extensions.ms-vscode.cpptools
-    vscode-extensions.donjayamanne.githistory
-    vscode-extensions.file-icons.file-icons
+    vscode-fhs
+    #vscode-with-extensions
+    #vscode-extensions.ms-vsliveshare.vsliveshare 
+    #vscode-extensions.ms-python.python
+    #vscode-extensions.ms-vscode.cpptools
+    #vscode-extensions.donjayamanne.githistory
+    #vscode-extensions.file-icons.file-icons
   ];
 }
