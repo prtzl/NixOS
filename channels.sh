528054b3991c0bf9d6f6285417d306dd2ea035cf
prtzl <matej.blagsic@protonmail.com>
2022-03-04 14:49:57 +0100

    Enable nix-flakes and add unstable channel, add jlink-pack as external package

--- channels.sh
+++ channels.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env sh
-sudo nix-channel --add https://nixos.org/channels/nixos-21.11 nixos-21.11
+sudo nix-channel --add https://nixos.org/channels/nixos-21.11 nixos
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
 sudo nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 sudo nix-channel --update
