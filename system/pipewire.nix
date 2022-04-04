59f22fe355866e32b7f2274c6553b1adb7cf48de
prtzl <matej.blagsic@protonmail.com>
2022-04-04 19:24:38 +0200

    Update sound

--- system/pipewire.nix
+++ system/pipewire.nix
@@ -1,18 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    pipewire
-    qjackctl
-  ];
-
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     media-session.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
   };
 
   sound.enable = true;
