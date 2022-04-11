3cfd9415cb6f4179803a967e8ebbeaf0b0269aa2
prtzl <matej.blagsic@protonmail.com>
2022-04-11 19:24:10 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    #jlink.url = "github:prtzl/jlink-pack";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell =
+          pkgs.mkShell { nativeBuildInputs = with pkgs; [ nix nixfmt ]; };
+      });
+}
