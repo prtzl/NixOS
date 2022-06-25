8c12eb8b9eaa2b3d6808be93e105d8c618701082
prtzl <matej.blagsic@protonmail.com>
2022-06-25 13:06:26 +0200

    Add force

--- prepare-disk.sh
+++ prepare-disk.sh
@@ -55,8 +55,8 @@ partitionEfi()
     parted -s $DISK mkpart primary 2560MiB 100%
     # Format
     mkfs.fat -F 32 -n boot ${PART}1
-    mkswap -L swap ${PART}2
-    mkfs.ext4 -L nixos ${PART}3
+    mkswap -F -L swap ${PART}2
+    mkfs.ext4 -F -L nixos ${PART}3
     # Mount and enable swap
     mount /dev/disk/by-label/nixos /mnt
     mkdir -p /mnt/boot/efi
@@ -70,8 +70,8 @@ partitionBios()
     parted -s $DISK mkpart primary linux-swap 1MiB 512MiB
     parted -s $DISK mkpart primary 512MiB 100%
     # Format
-    mkswap -L swap ${PART}1
-    mkfs.ext4 -L nixos ${PART}2
+    mkswap -F -L swap ${PART}1
+    mkfs.ext4 -F -L nixos ${PART}2
     # Mount and enable swap
     mount /dev/disk/by-label/nixos /mnt
     swapon /dev/disk/by-label/swap
