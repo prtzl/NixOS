0fff0db0ec5d894a3be327d75159106d27174d3c
prtzl <matej.blagsic@protonmail.com>
2022-07-07 23:54:00 +0200

    Remove result after update, now use /etc/nixos as link to this repository directory (/home/matej/nixos)

--- system/pkgs/nixos-update.sh
+++ system/pkgs/nixos-update.sh
@@ -8,7 +8,7 @@ function peval()
     fi
 }
 
-peval cd ~/NixOS
+peval cd /etc/nixos
 peval nix flake update
 peval nixos-rebuild build --flake . --impure "$@"
 peval nvd diff /run/current-system result
@@ -16,6 +16,7 @@ read -p "Perform switch (sudo)? [Y/n] " answer
 if [[ "$answer" == [yY] ]]; then
     echo Applying update!
     peval sudo result/bin/switch-to-configuration switch
+    peval rm result
     peval sudo nix-collect-garbage -d
     echo Update finished!
 else
