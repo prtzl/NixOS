900cc02012849ff97f590a94c0bdb3baaacae4f2
prtzl <matej.blagsic@protonmail.com>
2022-07-10 11:50:41 +0200

    'switch-to-configuration switch' doesn't apply derivation after reboot (missing)

--- system/pkgs/nixos-update.sh
+++ system/pkgs/nixos-update.sh
@@ -15,7 +15,9 @@ peval nvd diff /run/current-system result
 read -p "Perform switch (sudo)? [Y/n] " answer
 if [[ "$answer" == [yY] ]]; then
     echo Applying update!
-    peval sudo result/bin/switch-to-configuration switch
+    peval sudo nixos-rebuild switch --flake . --impure "$@"
+    # new derivation is applied but not after reboot - ?
+    # peval sudo result/bin/switch-to-configuration switch
     peval rm result
     peval sudo nix-collect-garbage -d
     echo Update finished!
