cfd79d2ad0292e3fd83d9fcdf6121d41383d9499
prtzl <matej.blagsic@protonmail.com>
2022-07-19 16:34:19 +0200

    vscode fuckery again

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,15 +4,16 @@ let
   unstable = pkgs.unstable;
 in {
   home.packages = with pkgs; [
-    (unstable.vscode-with-extensions.override {
-      vscodeExtensions = with unstable.vscode-extensions; [
-        arrterian.nix-env-selector
-        jnoortheen.nix-ide
-        ms-azuretools.vscode-docker
-        ms-vsliveshare.vsliveshare
-        vscodevim.vim
-        ms-vscode.cpptools
-      ];
-    })
+#    (unstable.vscode-with-extensions.override {
+#      vscodeExtensions = with unstable.vscode-extensions; [
+#        arrterian.nix-env-selector
+#        jnoortheen.nix-ide
+#        ms-azuretools.vscode-docker
+#        ms-vsliveshare.vsliveshare
+#        vscodevim.vim
+#        ms-vscode.cpptools
+#      ];
+#    })
+    vscode-fhs
   ];
 }
