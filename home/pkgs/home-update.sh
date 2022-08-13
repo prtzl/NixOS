bbb27dd4f5ad459bc99f4cc8717320e398256935
prtzl <matej.blagsic@protonmail.com>
2022-08-13 19:49:30 +0200

    Update home-update script

--- home/pkgs/home-update.sh
+++ home/pkgs/home-update.sh
@@ -8,21 +8,38 @@ function peval()
     fi
 }
 
-flake=$(id -un)-$(hostname)
+# Default location - for me, /etc/nixos is a symlink to git repository
+flake_dir=/etc/nixos
+home_derivation=$USER-$(hostname)
 
-peval cd /etc/nixos
-peval nix flake update
-peval home-manager build --flake .\#$flake --impure "$@"
-peval nvd diff $HOME/.nix-profile result
-read -p "Perform switch? [Y/n] " answer
+# Remove arguments ment for this script and only pass ARGS to other tools
+declare -a ARGS
+for var in "$@"; do
+    if [[ "$var" == "-r" ]]; then
+        update_flake_lock="true"
+        continue;
+    elif [[ "$var" = "-h" || "$var" == "--help" ]]; then
+        echo "This script updates lock file, builds derivation, shows update diff and applies it.\nAdd option '-r' to skip lock file update (just rebuild)."
+        exit 0
+    fi
+    ARGS[${#ARGS[@]}]="$var"
+done
+
+peval cd $flake_dir
+if [[ "$update_flake_lock" == "true" ]]; then
+    echo "Updating flake.lock!"
+    peval nix flake update
+fi
+
+echo "Building derivation!"
+peval home-manager build --flake .\#$home_derivation --impure "$ARGS"
+peval nvd diff /nix/var/nix/profiles/per-user/$USER/home-manager result
+read -p "Perform switch? [y/Y] " answer
 if [[ "$answer" == [yY] ]]; then
     echo Applying update!
-    peval home-manager switch --flake .\#$flake --impure "$@"
-    # new derivation is applied but not after reboot - ?
-    # peval sudo result/bin/switch-to-configuration switch
-    peval rm result
-    #peval sudo nix-collect-garbage -d
+    peval ./result/activate
     echo Update finished!
 else
     echo Update canceled!
 fi
+peval rm result
