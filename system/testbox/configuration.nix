ed6284f565c3b6952ccf5f5f618cfe798d4880da
prtzl <matej.blagsic@protonmail.com>
2022-08-29 15:33:02 +0200

    Add test system - settings the same as nixbox, will change that for a virtual machine later

---
+++ system/testbox/configuration.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  # Additional configuration
+  imports = [
+      ./hardware-configuration.nix
+      ../configuration_basic.nix
+  ];
+
+  # Networking - check your interface name enp<>s0
+  networking = {
+    hostName = "nixbox";
+    #networkmanager.enable = true;
+    #useNetworkd = true;
+    useDHCP = false;
+    interfaces.enp9s0.useDHCP = true;
+  };
+  
+  # Set your time zone - where are you ?
+  location.provider = "geoclue2";
+  time.timeZone = "Europe/Ljubljana";
+
+  # Select internationalisation properties
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = { font = "Lat2-Terminus16"; };
+
+# User sh$t
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users = {
+      test = {
+        isNormalUser = true;
+	    isSystemUser = false;
+        createHome = true;
+        extraGroups = [ "wheel" "networkmanager" "dialout" "audio" "video" "usb" ];
+      };
+    };
+  };
+}
+
---
+++ system/testbox/hardware-configuration.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+  boot = {
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
+
+  # More stuff
+  boot.kernelParams = [
+      "noibrs"
+      "noibpb"
+      "nopti"
+      "nospectre_v2"
+      "nospectre_v1"
+      "l1tf=off"
+      "nospec_store_bypass_disable"
+      "no_stf_barrier"
+      "mds=off"
+      "tsx=on"
+      "tsx_async_abort=off"
+      "mitigations=off"
+    ];
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = lib.mkDefault "performance";
+  };
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # Won't compile in nixos 22.05
+#  hardware.openrazer = {
+#    enable = true;
+#    users = [ "matej" ];
+#  };
+}
