2404594ca16d82a1b26e542d7f76285b4299f469
prtzl <matej.blagsic@protonmail.com>
2022-08-29 13:57:37 +0200

    Fix nvim packages - bug

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,11 +1,44 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  plugins = {
+  vimPlugins = {
     stable = pkgs.vimPlugins;
     unstable = pkgs.unstable.vimPlugins;
     local = pkgs.local.vimPlugins;
   };
+  loadPlugin = plugin: ''
+    set rtp^=${plugin}
+    set rtp+=${plugin}/after
+  '';
+  unlines = lib.concatStringsSep "\n";
+  loadPlugins = ps: lib.pipe ps [ (builtins.map loadPlugin) unlines ];
+  plugins = with vimPlugins.unstable; [
+    jellybeans-vim
+    vim-cpp-enhanced-highlight
+    base16-vim
+    coc-clangd
+    coc-fzf
+    coc-git
+    coc-json
+    coc-nvim
+    delimitMate
+    fzf-lsp-nvim
+    fzfWrapper
+    incsearch-vim
+    neoformat
+    nerdtree
+    nerdtree-git-plugin
+    tagbar
+    vim-airline
+    vim-code-dark
+    vim-devicons
+    vim-nerdtree-syntax-highlight
+    vim-nix
+    vim-pug
+    vim-vue
+    vimtex
+    (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars)) # syntax for everything
+  ];
 in {
   programs.neovim = {
     enable = true;
@@ -14,33 +47,12 @@ in {
     vimdiffAlias = true;
     withNodeJs = true;
     withPython3 = true;
-    plugins = with plugins.unstable; [
-      jellybeans-vim
-      vim-cpp-enhanced-highlight
-      base16-vim
-      coc-clangd
-      coc-fzf
-      coc-git
-      coc-json
-      coc-nvim
-      delimitMate
-      fzf-lsp-nvim
-      fzfWrapper
-      incsearch-vim
-      neoformat
-      nerdtree
-      nerdtree-git-plugin
-      tagbar
-      vim-airline
-      vim-code-dark
-      vim-devicons
-      vim-nerdtree-syntax-highlight
-      vim-nix
-      vim-pug
-      vim-vue
-      vimtex
-      (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars)) # syntax for everything
-    ];
-    extraConfig = builtins.readFile ./dotfiles/init.vim;
+    extraConfig = ''
+      " Workaround for broken handling of packpath by vim8/neovim for ftplugins -- see https://github.com/NixOS/nixpkgs/issues/39364#issuecomment-425536054 for more info
+      filetype off | syn off
+      ${loadPlugins plugins}
+      filetype indent plugin on | syn on
+      ${builtins.readFile ./dotfiles/init.vim}
+    '';
   };
 }
