6b2a52c468137876336116fb066fb9c96dfacf20
prtzl <matej.blagsic@protonmail.com>
2022-08-31 20:45:32 +0200

    Jlink package now works with udev, no need for explicit include

--- system/udev.nix
+++ system/udev.nix
@@ -9,10 +9,6 @@ KERNEL=="*", SUBSYSTEMS=="usb", MODE="0664", GROUP="usb"
 
 # RS232 devucesm yee
 SUBSYSTEMS=="usb", KERNEL=="ttyUSB[0-9]*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="sensors/ftdi_%s{serial}", GROUP="dialout"
-
-# JLINK - include rules manualy because somehow included jlink rules by the package
-# are not used - even if they are placed in /etc/udev/rules.d/
-${builtins.readFile "${pkgs.jlink}/lib/udev/rules.d/99-jlink.rules"}
     '';
   };
 }   
