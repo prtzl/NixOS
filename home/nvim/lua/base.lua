dc62fa73bb231c04c8d9c28f3dda86325a716049
prtzl <matej.blagsic@protonmail.com>
2022-10-01 15:17:45 +0200

    Config files preview window but could not figure out others.

--- home/nvim/lua/base.lua
+++ home/nvim/lua/base.lua
@@ -1,4 +1,4 @@
--- Base gui
+-- base gui
 vim.opt.number = true
 vim.opt.ruler = true
 vim.opt.showcmd = true
@@ -75,12 +75,11 @@ vim.cmd([[
 
 
     " Fuzzy find
-    let $BAT_THEME="OneHalfDark"
     let g:fzf_layout = { 'window': { 'width': 0.9, 'height': 0.9 } }
-    let $FZF_DEFAULT_COMMAND="rg --files --ignore-case --sort path"
-    let $FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --layout reverse --margin=1,4 --preview 'bat --color=always --style=numbers,grid,header-filename --line-range :300 {}'"
+    let $FZF_DEFAULT_OPTS="--prompt ':D' --ansi --preview-window 'right:60%' --layout reverse --margin=1,4 --preview 'bat --color=always --style=numbers,grid,header-filename --line-range :300 {}'"
+    let $FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git' --glob '!.cache' --glob '!.direnv' --sort path"
     command! -bang -nargs=? -complete=dir Files
-         \ call fzf#vim#files(<q-args>, fzf#vim#with_preview(), <bang>0)
+        \ call fzf#vim#files(<q-args>, { 'options': "--prompt ':D' --ansi --preview-window 'right:60%' --layout reverse --margin=1,4 --preview 'bat --color=always --style=numbers,grid,header-filename --line-range :300 {}'"}, <bang>0)
 
     " Print opened file in tmux bar
     if exists('$TMUX')
