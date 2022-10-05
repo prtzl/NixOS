0c1a44faee3ec89077282af75e24be15f8492b62
prtzl <matej.blagsic@protonmail.com>
2022-10-05 14:40:24 +0200

    nvim: organize cmp sources, add spell toggle

--- home/nvim/plugin/spell.lua
+++ home/nvim/plugin/spell.lua
@@ -2,3 +2,13 @@
 vim.opt.spell = false
 vim.opt.spelllang = { 'en_us' }
 
+vim.g.toggleSpell = function()
+    local isEnabled = vim.opt.spell:get()
+    if isEnabled then
+        vim.opt.spell = false
+        print("Spell disabled!")
+    else
+        vim.opt.spell = true
+        print("Spell enabled!")
+    end
+end
