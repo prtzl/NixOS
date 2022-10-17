7b04ea20018903ced20f92d22c02df3f35ca0ad8
prtzl <matej.blagsic@protonmail.com>
2022-10-17 13:06:54 +0200

    dconf: add power applet

--- home/dconf.nix
+++ home/dconf.nix
@@ -10,7 +10,8 @@ let
   backgroundPath = "${config.home.homeDirectory}/.background";
   lockscreenPath = "${config.home.homeDirectory}/.lockscreen";
   blackPath = "${config.home.homeDirectory}/.black";
-in {
+in
+{
   home.file.".save-windows.sh".source = ./local-pkgs/save.sh;
   home.file.".load-windows.sh".source = ./local-pkgs/load.sh;
 
@@ -19,8 +20,8 @@ in {
       desklet-snap-interval = 25;
       desktop-effects-workspace = false;
       enable-vfade = false;
-      enabled-applets = [ "panel1:left:0:menu@cinnamon.org:0" "panel1:left:2:grouped-window-list@cinnamon.org:2" "panel1:right:1:systray@cinnamon.org:3" "panel1:right:2:xapp-status@cinnamon.org:4" "panel1:center:0:notifications@cinnamon.org:5" "panel1:right:4:printers@cinnamon.org:6" "panel1:right:5:removable-drives@cinnamon.org:7" "panel1:right:6:keyboard@cinnamon.org:8" "panel1:right:8:network@cinnamon.org:10" "panel1:right:9:sound@cinnamon.org:11" "panel1:center:0:calendar@cinnamon.org:13" "panel1:right:0:workspace-switcher@cinnamon.org:14" ];
-      enabled-desklets = [];
+      enabled-applets = [ "panel1:left:0:menu@cinnamon.org:0" "panel1:left:2:grouped-window-list@cinnamon.org:2" "panel1:right:1:systray@cinnamon.org:3" "panel1:right:2:xapp-status@cinnamon.org:4" "panel1:center:0:notifications@cinnamon.org:5" "panel1:right:4:printers@cinnamon.org:6" "panel1:right:5:removable-drives@cinnamon.org:7" "panel1:right:6:keyboard@cinnamon.org:8" "panel1:right:8:network@cinnamon.org:10" "panel1:right:9:sound@cinnamon.org:11" "panel1:center:0:calendar@cinnamon.org:13" "panel1:right:0:workspace-switcher@cinnamon.org:14" "panel1:right:9:power@cinnamon.org:15" ];
+      enabled-desklets = [ ];
       next-applet-id = 15;
       panel-edit-mode = false;
       panel-zone-symbolic-icon-sizes = "[{\"panelId\": 1, \"left\": 28, \"center\": 28, \"right\": 16}]";
@@ -93,7 +94,7 @@ in {
     };
 
     "org/cinnamon/desktop/keybindings/media-keys" = {
-      terminal = [];
+      terminal = [ ];
     };
 
     "org/cinnamon/desktop/keybindings/wm" = {
