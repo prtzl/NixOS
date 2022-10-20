5e7fbf82fd327f2b0f2c3d82134c957a41026e9c
prtzl <matej.blagsic@protonmail.com>
2022-10-20 09:41:35 +0200

    Add signal-desktop for work laptop, update home script to also add links for desktop icons

--- home/packages/dotfiles/home-update.sh
+++ home/packages/dotfiles/home-update.sh
@@ -38,6 +38,7 @@ home_derivation=${NIX_HOME_DERIVATION:-$USER-$(hostname)}
 # Check for flake dir in nixos or non-nixos variables, else default in ~/.config/nixpkgs
 if checkForFlake "$NIX_FLAKE_DIR"; then
     flake_dir="$NIX_FLAKE_DIR"
+    system="nixos"
 elif checkForFlake "$NIX_FLAKE_DIR_HOME"; then
     flake_dir="$NIX_FLAKE_DIR_HOME"
 else
@@ -67,14 +68,31 @@ info "Building derivation!"
 peval home-manager build --flake .\#"$home_derivation" --impure "$ARGS"
 peval nvd diff /nix/var/nix/profiles/per-user/$USER/home-manager result
 read -p "Perform switch? [y/Y] " answer
+answer="y"
 if [[ "$answer" == [yY] ]]; then
     info Applying update!
     if ! peval ./result/activate; then
         rm result
         fatal "Failed to activate!"
     fi
+
+    info Linking desktop items
+    if [[ "$system" != "nixos" ]]; then
+        src_path=~/.nix-profile/share/applications
+        dest_path=~/.local/share/applications
+        for f in $src_path/*; do
+            fname=$(basename $f)
+            fsrc=$src_path/$fname
+            fdest=$dest_path/$fname
+            if [ -f "$fdest" ]; then
+                rm -f $fdest
+            fi
+            ln -s $fsrc $fdest
+        done
+    fi
+
     info Update finished!
 else
     info Update canceled!
 fi
-peval rm result
+peval rm -f result
