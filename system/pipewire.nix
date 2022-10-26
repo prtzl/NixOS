ef08fcddbdd937d1924f6e0925a52e90d63d138a
prtzl <matej.blagsic@protonmail.com>
2022-10-26 18:01:03 +0200

    pipewire: now adjusts output sample rate base od media played, cool :O

--- system/pipewire.nix
+++ system/pipewire.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ qjackctl ];
+  environment.systemPackages = with pkgs; [ qpwgraph ];
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -9,34 +9,22 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
+    jack.enable = false;
     config.pipewire = {
       "context.properties" = {
-        "link.max-buffers" = 32;
+        "link.max-buffers" = 64;
         "log.level" = 2;
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 32;
+        "default.clock.rate" = 48000; # all signals are converted to this sample rate, and then back to what device supports
+        "default.clock.allowed-rates" = [ 44100 48000 88200 96000 176400 192000 352800 384000 ];
         "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 32;
+        "default.clock.max-quantum" = 4096;
+        "default.clock.quantum" = 1024;
+        "defualt.clock.quantum-limit" = 4096;
+        "clock.power-of-two-quantum" = true; # more efficient
         "core.daemon" = true;
         "core.name" = "pipewire-0";
+
       };
-    };
-    media-session.config.alsa-monitor = {
-      rules = [
-        {
-          # SMSL SU-9 : out, Focusrite iTrack Solo : in/out
-          matches = [{ "node.name" = "alsa_output.usb-SMSL_SMSL_USB_AUDIO*"; } { "node.name" = "alsa_output.usb-Focusrite_iTrack_Solo*"; } { "node.name" = "alsa_input.usb-Focusrite_iTrack_Solo*"; }];
-          actions = {
-            update-props = {
-              "audio.format" = "S32LE";
-              "audio.rate" = 96000; # for USB soundcards it should be twice your desired rate
-              "api.alsa.period-size" = 32; # defaults to 1024, tweak by trial-and-error
-              "api.alsa.disable-batch" = false; # generally, USB soundcards use the batch mode
-            };
-          };
-        }
-      ];
     };
   };
 
