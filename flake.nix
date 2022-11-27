9e84b816b87ac2dbd64f31e8ae620a74081314aa
prtzl <matej.blagsic@protonmail.com>
2022-11-27 23:39:46 +0100

    Move wrap function, test if working, misc

--- flake.nix
+++ flake.nix
@@ -32,32 +32,11 @@
 
       mkFree = drv: drv.overrideAttrs (attrs: { meta = attrs.meta // { license = ""; }; });
 
-      glWrapIntel = { pkg, deps ? [ ] }: pkgs.stdenv.mkDerivation rec {
-        pname = pkg.pname + "-glwrap";
-        version = pkg.version;
-        src = pkg;
-        nativeBuildInputs = [ pkg pkgs.nixgl.nixGLIntel ] ++ deps;
-        installPhase = ''
-          mkdir -p $out/bin
-          for d in `find $src -maxdepth 1 -type d ! -path $src | grep -v bin`; do
-            ln -s $d $out/$(basename $d)
-          done
-          for fpath in `find $src/bin -type f`; do
-            f=$(basename $fpath)
-            fold=$f"_base"
-            ln -s $fpath $out/bin/$fold
-            touch $out/bin/$f
-            chmod +x $out/bin/$f
-            echo "${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel $out/bin/$fold" > $out/bin/$f
-          done
-        '';
-      };
-
       stableOverlay = self: super: {
         unstable = pkgs-unstable;
         jlink = mkFree inputs.jlink-pack-stable.defaultPackage.${system};
         patched = pkgs-matej;
-        glWrapIntel = glWrapIntel;
+        tools = import tools/wrappers.nix;
         signal-desktop = pkgs-matej.signal-desktop;
       };
 
---
+++ tools/wrappers.nix
@@ -0,0 +1,26 @@
+{ stdenv, nixgl }:
+
+let
+in
+{
+  glWrapIntel = { pkg, deps ? [ ] }: stdenv.mkDerivation rec {
+    pname = pkg.pname + "-glwrap";
+    version = pkg.version;
+    src = pkg;
+    nativeBuildInputs = [ pkg nixgl.nixGLIntel ] ++ deps;
+    installPhase = ''
+      mkdir -p $out/bin
+      for d in `find $src -maxdepth 1 -type d ! -path $src | grep -v bin`; do
+        ln -s $d $out/$(basename $d)
+      done
+      for fpath in `find $src/bin -type f`; do
+        f=$(basename $fpath)
+        fold=$f"_base"
+        ln -s $fpath $out/bin/$fold
+        touch $out/bin/$f
+        chmod +x $out/bin/$f
+        echo "${nixgl.nixGLIntel}/bin/nixGLIntel $out/bin/$fold" > $out/bin/$f
+      done
+    '';
+  };
+}
