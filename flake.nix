c4cffb5125390e4d03dd59ea1b6285fd82d5f30f
prtzl <matej.blagsic@protonmail.com>
2022-12-06 11:59:44 +0100

    Update home-manager to 22.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     jlink-pack-stable = {
