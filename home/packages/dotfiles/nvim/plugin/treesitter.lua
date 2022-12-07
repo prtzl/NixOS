6c964f6fed687346f5e3507d3e0df9acab7ef7a4
prtzl <matej.blagsic@protonmail.com>
2022-12-07 18:18:41 +0100

    nvim: disable plugin on large files

--- home/packages/dotfiles/nvim/plugin/treesitter.lua
+++ home/packages/dotfiles/nvim/plugin/treesitter.lua
@@ -9,6 +9,9 @@ require 'nvim-treesitter.configs'.setup {
     },
     rainbow = {
         enable = true,
+        disable = function(lang, bufnr) -- Disable in large buffers
+            return vim.api.nvim_buf_line_count(bufnr) > 5000
+        end,
         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
