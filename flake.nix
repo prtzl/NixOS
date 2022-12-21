aec3ba278f9877d62e2a232ab65b8a10caa5fd85
prtzl <matej.blagsic@protonmail.com>
2022-12-21 14:21:13 +0100

    Move glWrapIntel to a folder

--- flake.nix
+++ flake.nix
@@ -35,27 +35,6 @@
 
       mkFree = drv: drv.overrideAttrs (attrs: { meta = attrs.meta // { license = ""; }; });
 
-      glWrapIntel = { pkg, deps ? [ ] }: pkgs.stdenv.mkDerivation rec {
-        pname = pkg.pname + "-glwrap";
-        version = pkg.version;
-        src = pkg;
-        nativeBuildInputs = [ pkg pkgs.nixgl.nixGLIntel ] ++ deps;
-        installPhase = ''
-          mkdir -p $out/bin
-          for d in `find $src -maxdepth 1 -type d ! -path $src | grep -v bin`; do
-            ln -s $d $out/$(basename $d)
-          done
-          for fpath in `find $src/bin -type f`; do
-            f=$(basename $fpath)
-            fold=$f"_base"
-            ln -s $fpath $out/bin/$fold
-            touch $out/bin/$f
-            chmod +x $out/bin/$f
-            echo "${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel $out/bin/$fold" > $out/bin/$f
-          done
-        '';
-      };
-
       stableOverlay = self: super: {
         # Packages
         unstable = pkgs-unstable;
@@ -63,7 +42,7 @@
         patched = pkgs-matej;
         # Stable package overrides/additions
         jlink = mkFree inputs.jlink-pack-stable.defaultPackage.${system};
-        glWrapIntel = glWrapIntel;
+        glWrapIntel = (import ./nix/nixgl.nix { inherit pkgs; }).glWrapIntel;
         signal-desktop = pkgs-matej.signal-desktop;
         stm32cubemx = pkgs-matej.stm32cubemx;
       };
