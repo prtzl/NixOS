fde952cf59f38bf041f132f618f499a7d0ba445a
prtzl <matej.blagsic@protonmail.com>
2023-01-26 16:35:33 +0100

    Fix alacritty config - when did it change? Why did it work untill now?

--- home/packages/fonts.nix
+++ home/packages/fonts.nix
@@ -4,11 +4,11 @@ let
   mkInt = lib.hm.gvariant.mkUint32;
 in
 {
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig.enable = lib.mkForce true;
 
   home.packages = with pkgs; [
     (nerdfonts.override {
-      fonts = [ "Cousine" "FiraCode" "RobotoMono" "SourceCodePro" "NerdFontsSymbolsOnly" ];
+      fonts = [ "Cousine" "FiraCode" "RobotoMono" "SourceCodePro" ];
     })
   ];
 }
