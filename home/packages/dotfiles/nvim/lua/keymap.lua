de7aad0e63bd09ec9b6a45c03f9f76909150f7e3
prtzl <matej.blagsic@protonmail.com>
2023-02-07 10:14:51 +0100

    Fix spelling

--- home/packages/dotfiles/nvim/lua/keymap.lua
+++ home/packages/dotfiles/nvim/lua/keymap.lua
@@ -46,7 +46,7 @@ map('s', '<c-k>', '<cmd>lua require"luasnip".jump(-1)<CR>', nil)
 -- FZF
 map('n', '<c-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>', nil)
 map('n', '<c-a>',
-    '<cmd>lua require("telescope.builtin").find_files({no_ignore = true, no_gnore_parent = true, hidden = true})<cr>',
+    '<cmd>lua require("telescope.builtin").find_files({no_ignore = true, no_ignore_parent = true, hidden = true})<cr>',
     nil)
 map('n', '<c-g>', '<cmd>lua require("telescope.builtin").live_grep()<cr>', nil)
 map('n', '<c-b>', '<cmd>lua require("telescope.builtin").buffers()<cr>', nil)
