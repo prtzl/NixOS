ff163edd7506a23eb3336721ef80ebd6806704bd
prtzl <matej.blagsic@protonmail.com>
2023-08-23 23:16:42 +0200

    Pipewire config in a etc config file. Let's see for now.

--- system/packages/pipewire.nix
+++ system/packages/pipewire.nix
@@ -28,6 +28,22 @@
     # };
   };
 
+  environment.etc."pipewire/pipewire.conf.d/pipewire.conf".text = ''
+    context.properties = {
+      "link.max-buffers" = 64;
+      "log.level" = 2;
+      "default.clock.rate" = 48000; # all signals are converted to this sample rate, and then back to what device supports
+      "default.clock.allowed-rates" = [ 44100 48000 88200 96000 176400 192000 352800 384000 ];
+      "default.clock.min-quantum" = 32;
+      "default.clock.max-quantum" = 4096;
+      "default.clock.quantum" = 1024;
+      "defualt.clock.quantum-limit" = 4096;
+      "clock.power-of-two-quantum" = true; # more efficient
+      "core.daemon" = true;
+      "core.name" = "pipewire-0";
+    };
+  '';
+
   sound.enable = true;
   hardware.pulseaudio.enable = false;
 }
