e72b9ef02e70d25a6c2fdba25827b4630e69703f
prtzl <matej.blagsic@protonmail.com>
2023-11-22 23:58:28 +0100

    Update & monitored on nonNixos, will it work?

--- home/packages/home_basic.nix
+++ home/packages/home_basic.nix
@@ -6,7 +6,7 @@ in
 {
   nix =
     if notNixos then {
-      package = inputs.nix-monitored.package.${pkgs.system}.default;
+      # package = inputs.nix-monitored.package.${pkgs.system}.default;
       registry = {
         nixpkgs.flake = inputs.nixpkgs-stable;
         unstable.flake = inputs.nixpkgs-unstable;
