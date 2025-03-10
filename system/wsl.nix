f5001ef8e717a8032b6f3b8deed20815c7efc9a3
BMATE <matej.blagsic-ext@hexagon.com>
2025-03-10 09:20:13 +0000

    Format flake, create wsl.nix configuration, move stuff from configuration_basic.nix to individual builds.

---
+++ system/wsl.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, lib, modulesPath, ... }:
+
+{
+  # Additional configuration
+  imports = [ ./packages/configuration_basic.nix <nixos-wsl/modules> ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+
+  system.stateVersion = "24.11";
+
+  # set top 8x2 = packages that do build will take some time, but oh well
+  nix.settings = {
+    # max-jobs = maximum packages built at once
+    max-jobs = 2;
+    # cores = maximum threads used by a job/package
+    cores = 8;
+  };
+
+  # Networking - check your interface name enp<>s0
+  networking = {
+    hostName = "nixbox";
+    interfaces.eth0.useDHCP = true;
+  };
+
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+
+  # Set your time zone - where are you ?
+  time.timeZone = "Europe/Ljubljana";
+
+  # Select internationalisation properties
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = { font = "Lat2-Terminus16"; };
+
+  # User sh$t
+  programs.zsh.enable = true;
+  users = {
+    users = {
+      nixos = {
+        isNormalUser = true;
+        isSystemUser = false;
+        createHome = true;
+        extraGroups = [
+          "wheel"
+          "libvirtd"
+          "networkmanager"
+          "dialout"
+          "audio"
+          "video"
+          "usb"
+          "podman"
+          "docker"
+          "openrazer"
+          "kvm"
+          "adbusers"
+        ];
+      };
+    };
+  };
+}
