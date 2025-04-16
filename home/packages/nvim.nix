a8bfb8d3f612ba7fc74bdd157c531aadbf850802
prtzl <matej.blagsic@protonmail.com>
2025-04-16 09:33:59 +0200

    Cleanup, refresh

--- home/packages/nvim.nix
+++ home/packages/nvim.nix
@@ -1,8 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 
-let mynvim = pkgs.nvimnix.packages.${pkgs.system}.default;
-in {
-  # My fancy pantsy portable nvimnix does not bring it's own programs, so keep this here for now
-  # home.packages = [ mynvim ];
+{
   programs.nvimnix.enable = true;
 }
