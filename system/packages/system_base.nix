55dffe29fa7b1fe678a6e37599ac83aeb5b23a60
prtzl <matej.blagsic@protonmail.com>
2025-04-21 13:56:18 +0200

    Locale fun

--- system/packages/system_base.nix
+++ system/packages/system_base.nix
@@ -43,6 +43,7 @@ in {
   console.font = "iso02-12x22";
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "en_DK.UTF-8/UTF-8" ];
     extraLocaleSettings = { LC_TIME = "en_DK.UTF-8"; };
   };
 
