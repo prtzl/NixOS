9fc450479c84cb37e0c6bb73f4d1662dc3fa09e6
prtzl <matej.blagsic@protonmail.com>
2025-07-06 21:57:18 +0200

    Seems like xfce programs (just file explorer for now) need this to save settings.

--- system/packages/system_graphics.nix
+++ system/packages/system_graphics.nix
@@ -12,6 +12,9 @@
     withUWSM = true;
   };
 
+  # needed by xfce apps to save config - yikes, well, at least not gnome
+  programs.xfconf.enable = true;
+
   # Fixes electron apps in wayland, so I've read.
   environment.sessionVariables = { NIXOS_OZONE_WL = "1"; };
 
